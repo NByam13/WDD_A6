f6d3698c2ae543ce03932bc4b6bf052050ce9411
Tangent1al <y.dydxx.b@gmail.com>
2020-11-23 00:32:05 -0500

    Commented

--- MyOwnWebServer/MyOwnWebServer/WebServer.cs
+++ MyOwnWebServer/MyOwnWebServer/WebServer.cs
@@ -24,11 +24,27 @@ namespace MyOwnWebServer
         public int Port { get; set; }
         public IPAddress ServerIP { get; set; }
 
+
+        /////////////////////////////////////////
+        // Method       : WebServer (ctor)
+        // Description  : Constructor for the webserver class. The constructor calls on a method to set the command line
+        //              : arguments that were passed in to the right properties so that the server can start.
+        // Parameters   : string[] args : the string array of arguments
+        // Returns      : N/A
+        /////////////////////////////////////////
         public WebServer(string[] args)
         {
             SetProperties(args);
         }
 
+
+        /////////////////////////////////////////
+        // Method       : main
+        // Description  : In the main the program checks to see if the number of cmd line arguments is correct, if the user
+        //              : has asked for help, and how to proceed if the right arguments have been given.
+        // Parameters   : string[] args : command line arguments used to start the server, or give the user a help text
+        // Returns      : N/A
+        /////////////////////////////////////////
         static void Main(string[] args)
         {
             if(Help(args)) // if help is the first arg, then ignore any others, and print instructions to the cmd prompt
@@ -69,21 +85,28 @@ namespace MyOwnWebServer
         }
 
 
+        /////////////////////////////////////////
+        // Method       : StartServer
+        // Description  : A method to create and start a listener based on the Server's given IP and Port. The listener then waits to
+        //              : accept a tcp client.
+        // Parameters   : N/A
+        // Returns      : N/A
+        /////////////////////////////////////////
         public void StartServer()
         {
-            TcpListener listener = new TcpListener(ServerIP, Port);
-            listener.Start();
+            TcpListener listener = new TcpListener(ServerIP, Port); // create a listener on the given IP and Port
+            listener.Start(); // start the listener
 
-            while(run)
+            while(run) // wait for incoming connection requests until the run boolean is set to false. 
             {
-                if(!listener.Pending())
+                if(!listener.Pending()) // if there is not a connection pending continue to next iteration, therefore no blocking occurs
                 {
                     continue;
                 }
                 else
                 {
-                    TcpClient client = listener.AcceptTcpClient();
-                    HandleClient(client);
+                    TcpClient client = listener.AcceptTcpClient(); // when a connection is pending, connect to client
+                    HandleClient(client); // jump to a method to handle the client.
                     client.Close(); // Might not need this here if we close in Handle Client.
                 }
             }
@@ -92,13 +115,26 @@ namespace MyOwnWebServer
         }
 
 
+        /////////////////////////////////////////
+        // Method       : HandleClient
+        // Description  :
+        // Parameters   : TcpClient client : the client connection
+        // Returns      : N/A
+        /////////////////////////////////////////
         public void HandleClient(TcpClient client)
         {
             //
         }
 
 
-
+        /////////////////////////////////////////
+        // Method       : ParseArgs
+        // Description  : A method that checks for unknown arguments, and if there are none, breaks up the arguments into a list
+        //              : containing the key and the value.
+        // Parameters   : string[] args : the cmd line arguments
+        // Returns      : string[] err : if unknown commands are found in cmd line args, send back a list of the unknown commands
+        //              : string[] argArray : a complete list of valid cmd line arguments broken into alternating keys and values
+        /////////////////////////////////////////
         static public string[] ParseArgs(string[] args)
         {
             string[] err = new string[3];
@@ -117,37 +153,47 @@ namespace MyOwnWebServer
                 {
                     continue;
                 }
-                else
+                else // if the current arg is unrecognized, add it to the error list.
                 {
                     err[count] = arg;
                     count++;
                 }
             }
-            if(err[0] != null)
+            if(err[0] != null) // if the error list has anything in it, the first index will not be null
             {
-                return err;
+                return err; // return the unknown args
             }
 
-            string[] argArray = new string[kMaxSplitArgs];
+            string[] argArray = new string[kMaxSplitArgs]; // create an array of size 6 to hold the valid keys and values
             count = 0;
-            char splitChar = '=';
-            foreach (string arg in args)
+            char splitChar = '='; // we will split on = because that divides the key and value for all args
+            foreach (string arg in args) // iterate through all args
             {
                 string[] tmp;
                 int i = 0;
-                tmp = arg.Split(splitChar);
-                argArray[count] = tmp[i];
+                tmp = arg.Split(splitChar); // split on =
+                argArray[count] = tmp[i]; // add key to array
                 count++;
                 i++;
-                argArray[count] = tmp[i];
+                argArray[count] = tmp[i]; // add value to array
                 count++;
             }
-            return argArray;
+            return argArray; // return key value array
         }
 
+
+        /////////////////////////////////////////
+        // Method       : Help
+        // Description  : A method that checks to see if the user has asked for help regarding starting the web server.
+        //              : prints a list of key-value pairs that are required to start the server. It will only accept the help arg
+        //              : if it is the first and only argument. Having it amongst other arguments will not work.
+        // Parameters   : string[] args : the cmd line arg list
+        // Returns      : true : if help was given
+        //              : false : if help was not given
+        /////////////////////////////////////////
         static public bool Help(string[] args)
         {
-            if(args[0].ToLower() == "help")
+            if(args[0].ToLower() == "help" && args.Length == 1) // help arg only accepted when it is the first and only argument
             {
                 Console.WriteLine("|--- 3 Arguments are necessary to start the Web Server ---|\n");
                 Console.WriteLine("1. -webRoot=<path/to/web/data>");
@@ -158,23 +204,32 @@ namespace MyOwnWebServer
             else { return false; }
         }
 
-        public void SetProperties(string[] args)
+
+        /////////////////////////////////////////
+        // Method       : SetProperties
+        // Description  : A method to set the given cmd line arguments to the right properties. It iterates through an array attempting to
+        //              : to parse the right info out. By doing it this way, the properties can be set properly regardless of the order
+        //              : the cmd line arguments were given.
+        // Parameters   : string[] args : the array containing the split up cmd line arguments
+        // Returns      : N/A
+        /////////////////////////////////////////
+        private void SetProperties(string[] args)
         {
-            for(int i = 0; i < kMaxSplitArgs; i++)
+            for(int i = 0; i < args.Length; i++) // loop over entire array
             {
                 if(i%2 != 0) // the pertinent info will be at indeces 1, 3, 5
                 {
                     if (int.TryParse(args[i], out int tmpPort))
                     {
-                        Port = tmpPort;
+                        Port = tmpPort; // set port if the current value is an integer value
                     }
                     else if (IPAddress.TryParse(args[i], out IPAddress tmpIP))
                     {
-                        ServerIP = tmpIP;
+                        ServerIP = tmpIP; // set ServerIP if the current value is an IPAddress type
                     }
                     else
                     {
-                        DataPath = args[i];
+                        DataPath = args[i]; // if the value is neither an Int nor an IPAddress type, then it is the data path.
                     }
                 }
             }
