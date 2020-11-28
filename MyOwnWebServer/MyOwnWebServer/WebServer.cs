fc19fafa775658aac58bf5e9f098eaf81eb18c0c
NikProgramming <NikProgramming@users.noreply.github.com>
2020-11-28 16:56:00 -0500

    handle client, modified example

--- MyOwnWebServer/MyOwnWebServer/WebServer.cs
+++ MyOwnWebServer/MyOwnWebServer/WebServer.cs
@@ -116,7 +116,30 @@ namespace MyOwnWebServer
         /////////////////////////////////////////
         public void HandleClient(TcpClient client)
         {
-            //
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String data = null;
+
+            data = null;
+
+            // Get a stream object for reading and writing
+            NetworkStream stream = client.GetStream();
+
+            int i;
+
+            // Loop to receive all the data sent by the client.
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                // Translate data bytes to a ASCII string.
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
+
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                //do something with input recieved by user
+            }
+
+            // Shutdown and end connection
+            client.Close();
         }
 
 
