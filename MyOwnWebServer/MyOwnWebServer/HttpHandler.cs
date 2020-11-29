be7a640a639bfdc431ee583da56ff61f8ec45ed7
Tangent1al <y.dydxx.b@gmail.com>
2020-11-28 21:34:21 -0500

    Validate request done

--- MyOwnWebServer/MyOwnWebServer/HttpHandler.cs
+++ MyOwnWebServer/MyOwnWebServer/HttpHandler.cs
@@ -13,9 +13,34 @@ namespace MyOwnWebServer
             if(data.StartsWith("GET"))
             {
                 // this means it is a GET request
-                string[] splitData = data.Split(' ');
-                string path = splitData[1];
+                if(data.EndsWith("HTTP/1.1\r\n"))
+                {
+                    string[] splitData = data.Split(' ');
+                    string path = splitData[1];
+                    // Also need to check the last element is http/1.1 \r\n
+                    if (FileHandler.IsValidPath(path))
+                    {
+                        // is a valid header
+                        return true;
+                    }
+                    else
+                    {
+                        // is not a valid path
+                        return false;
+                    }
+                }
+                else
+                {
+                    // not a valid header if it does not have this
+                    return false;
+                }
             }
+            else
+            {
+                // we don't support post
+                return false;
+            }
+            
         }
     }
 }
