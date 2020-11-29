730e558aa021d050e956348ede7e2b1716962876
Tangent1al <y.dydxx.b@gmail.com>
2020-11-28 22:20:39 -0500

    created codes

--- MyOwnWebServer/MyOwnWebServer/HttpHandler.cs
+++ MyOwnWebServer/MyOwnWebServer/HttpHandler.cs
@@ -8,6 +8,22 @@ namespace MyOwnWebServer
 {
     static public class HttpHandler
     {
+        public struct HTTPCodes
+        {
+            const string OK = "200 OK";
+            const string NoContent = "204 No Content";
+            const string BadRequest = "400 Bad Request";
+            const string Forbidden = "403 Forbidden";
+            const string NotFound = "404 Not Found";
+            const string NotAllowed = "405 Method Not Allowed";
+            const string TeaPot = "418 I'm a Teapot";
+            const string TooMany = "429 Too Many Requests";
+            const string ServerErr = "500 Internal Server Error";
+            const string NotImplemented = "501 Not Implemented";
+            const string VersionErr = "505 HTTP Version Not Supported";
+
+        }
+
         public static bool ValidateRequest(string data)
         {
             if(data.StartsWith("GET"))
@@ -41,6 +57,11 @@ namespace MyOwnWebServer
                 return false;
             }
             
+        }
+
+        public static string BuildResponse()
+        {
+
         }
     }
 }
